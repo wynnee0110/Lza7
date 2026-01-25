57b2de9c283dd023cc5320550b3f2fdf601c882c
Wayne <wynnee0110@gmail.com>
2026-01-26 00:33:01 +0800

    error

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,7 +1,7 @@
 
 import Header from "../components/Header";
 import ProjectsSection from "../components/ProjectsSection";
-
+import Link from "next/link";
 
 export default function WorksPage() {
     return (
@@ -10,14 +10,13 @@ export default function WorksPage() {
 
 
         <div className="mt-15" >
-            <a href="/" className="inline-flex gap-0.5 ml-8 hover:text-gray-400 transition-all duration-300 ease-out 
+            <Link href="/" className="inline-flex gap-0.5 ml-8 hover:text-gray-400 transition-all duration-300 ease-out 
              hover:opacity-70">
                 <svg width="24" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M20 12H4M4 12L10 18M4 12L10 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
  </svg>
  <span className="px-2 mt-0.4">Go to Home</span>
-            </a>
-            
+            </Link>
 
             <ProjectsSection />
         </div>
