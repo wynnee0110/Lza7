7bb6fe3e506c3576864efd77847444ba9653fabb
Wayne <wynnee0110@gmail.com>
2026-02-20 04:01:49 +0800

    hey

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Page () {
     return (
         <div>
         <Header />
+        <Footer />
         </div>
         
 
